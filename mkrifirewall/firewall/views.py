997216d19b0b4afa67e87ee999a0d13357fc9fae
Adrian Tomasov <atomasov@redhat.com>
2020-04-04 15:54:38 +0200

    [django] form&view: addimg configuraiton form and view

--- mkrifirewall/firewall/views.py
+++ mkrifirewall/firewall/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.http import HttpResponse
 
+from firewall.forms import NFTablesConfigForm
 from firewall.sys_utils.nftables import NFTables
 
 
@@ -15,4 +17,16 @@ def stats(request):
 
 
 def rules(request):
-    return render(request, 'firewall/rules.html', {'conf': NFTables.get_current_configuration()})
+    print(request)
+    if request.method == 'POST':
+        form = NFTablesConfigForm(request.POST)
+        try:
+            NFTables.apply_current_cong(form['nft_config'])
+        except Exception as e:
+            messages.warning(request, "There is an error in your configuration!")
+        else:
+            messages.success(request, "Configuration successfully applied.")
+
+    else:  # == GET
+        form = NFTablesConfigForm()
+    return render(request, 'firewall/rules.html', {'form': form})
