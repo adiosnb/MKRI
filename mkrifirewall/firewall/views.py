a92933fb7fdfd5e3f694a2f1cf7b8102d8ad4bb9
Adrian Tomasov <atomasov@redhat.com>
2020-04-04 11:57:30 +0200

    [front-end] bck

--- mkrifirewall/firewall/views.py
+++ mkrifirewall/firewall/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from firewall.sys_utils.nftables import NFTables
+
+
 # Create your views here.
 
 def home(request):
     return render(request, 'firewall/home.html')
 
+
 def stats(request):
     return render(request, 'firewall/stats.html')
 
+
 def rules(request):
-    return render(request, 'firewall/rules.html')
+    return render(request, 'firewall/rules.html', {'conf': NFTables.get_current_configuration()})
