54c299f1dc7ce178dbe476e415aa78008c476ae3
Adrian Tomasov <atomasov@redhat.com>
2020-04-10 13:25:13 +0200

    [django] form initail is set in view

--- mkrifirewall/firewall/forms.py
+++ mkrifirewall/firewall/forms.py
@@ -1,9 +1,6 @@
 from django.forms import Form, Textarea, CharField
 
-from firewall.sys_utils.nftables import NFTables
-
 
 class NFTablesConfigForm(Form):
     # nft_config = Textarea(initial=NFTables.get_current_configuration())
-    nft_config = CharField(widget=Textarea(attrs={"rows": 30, 'class': 'form-control'}),
-                           initial=NFTables.get_current_configuration(), label="")
+    nft_config = CharField(widget=Textarea(attrs={"rows": 30, 'class': 'form-control'}), label="")
