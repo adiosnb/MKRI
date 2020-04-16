a15b17b8baac84ec2dab837eafe52a2c408eebdf
Adrian Tomasov <atomasov@redhat.com>
2020-04-16 11:24:37 +0200

    [view] add drop chart and refactor

--- mkrifirewall/firewall/views.py
+++ mkrifirewall/firewall/views.py
@@ -16,18 +16,33 @@ def home(request):
 
 
 def stats(request):
+    def get_max_key(data: dict):
+        """ Find maximal number in dict and return its key """
+        try:
+            max_key = next(iter(data.keys()))
+        except StopIteration:
+            return None
+        else:
+            for key, val in data.items():
+                if accepted[max_key] < val:
+                    max_key = key
+            return max_key
+
     global old_data
     old_data = process_nftables_data(NFTables.get_stats())
-    accepted = old_data[0]
-    chart1_max = next(iter(accepted.keys()))
-    for key, val in accepted.items():
-        if accepted[chart1_max] < val:
-            chart1_max = key
-    context = {
-        'chart1': accepted,
-        'chart1_max_key': chart1_max
-    }
-    return render(request, 'firewall/stats.html', context)
+    accepted, denied = old_data
+    chart1_max = get_max_key(accepted)
+    chart2_max = get_max_key(denied)
+    if chart1_max is not None or chart2_max is not None:
+        context = {
+            'chart1': accepted,
+            'chart2': denied,
+            'chart1_max_key': chart1_max,
+            'chart2_max_key': chart2_max,
+        }
+        return render(request, 'firewall/stats.html', context)
+    else:
+        return render(request, 'firewall/empty_stats.html', {})
 
 
 def get_traffic_stats(request):
