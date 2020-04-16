6d49e19bd2a8c9c7d2b506f90da60aceca1970ad
Adrian Tomasov <atomasov@redhat.com>
2020-04-16 10:51:18 +0200

    [utils] run cmd in shell, because of pipes

--- mkrifirewall/firewall/sys_utils/shell.py
+++ mkrifirewall/firewall/sys_utils/shell.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def run_command(cmd: str) -> (str, int):
-    command_handle = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    command_handle = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     command_handle.wait()
     command_handle.stdout.flush()
     return command_handle.stdout.read().decode(), command_handle.poll()
